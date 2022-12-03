5de042c36b99f9cde955c9d39abf1eccbe7d238a
antran1245 <antran1245@gmail.com>
2022-12-03 14:36:16 -0800

    completed the footer for  location comp

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import { Button, Row, Col } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
+import facebook from '../assets/icon-facebook.svg'
+import instagram from '../assets/icon-instagram.svg'
+import twitter from '../assets/icon-twitter.svg'
+import logo from '../assets/logo-dark.svg'
 import '../sass/location.scss'
 
 export default function Location() {
+    const navigate = useNavigate()
     return(
         <>
             <Row id="map">
-                <img src={require('../assets/desktop/image-map.png')} alt="desktop map" className='d-none d-lg-block'/>
-                <img src={require('../assets/tablet/image-map.png')} alt="tablet map" className='d-none d-md-block d-lg-none'/>
-                <img src={require('../assets/mobile/image-map.png')} alt="mobile map" className='d-block d-md-none'/>
-                <div className='button-group'>
+                <img src={require('../assets/desktop/image-map.png')} alt="desktop map" className='d-none d-md-block'/>
+                <img src={require('../assets/tablet/image-map.png')} alt="tablet map" className='d-none d-sm-block d-md-none'/>
+                <img src={require('../assets/mobile/image-map.png')} alt="mobile map" className='d-block d-sm-none'/>
+                <div className='button-group' onClick={() => {navigate('/')}}>
                     <span></span>
                     <Button>BACK TO HOME</Button>
                 </div>
@@ -29,6 +35,19 @@ export default function Location() {
                     <div className='info'>
                         <p>Our newly opened gallery is located near the Edward King House on 99 King Street, the Modern Art Gallery is free to all visitors and open seven days a week from 8am to 9pm.</p>
                     </div>
+                </Col>
+            </Row>
+            <Row id='location-footer'>
+                <Col md={3} lg={3} xl={2} className="title">
+                    <img src={logo} alt="logo" />
+                </Col>
+                <Col md={6} lg={5} xl={8} className="info">
+                    <p>The Modern Art Gallery is free to all visitors and open seven days a week from 8am to 9pm. Find us at 99 King Street, Newport, USA.</p>
+                </Col>
+                <Col md={3} lg={4} xl={2} className="icons">
+                    <img src={facebook} alt='facebook'/>
+                    <img src={instagram} alt='instagram'/>
+                    <img src={twitter} alt='twitter'/>
                 </Col>
             </Row>
         </>
